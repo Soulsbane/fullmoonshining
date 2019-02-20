12ed587aff05efc162e9da50ddac34fae07cbe41
Paul Crane <paul@acheronsoft.com>
2019-02-20 00:22:43 -0600

    First pass at modularizing the code.

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+require_once("pages/header.php");
+
+function isRaining()
+{
+	if (mt_rand(0, 24) > 3) {
+		return false;
+	}
+
+	return true;
+}
+
+function isNight()
+{
+	if (mt_rand(0, 24) > 12) {
+		return false;
+	}
+
+	return true;
+}
+
+if (isNight()) {// Night
+	if (isRaining()) {
+		header('Location: http://fullmoonshining.com/rain');
+	} else {
+		header('Location: http://fullmoonshining.com/higurashi');
+	}
+} else { // Day
+	if (isRaining()) {
+		header('Location: http://fullmoonshining.com/rain/index-day.html');
+	} else {
+		header('Location: http://fullmoonshining.com/natsu');
+	}
+}
+
+require_once("pages/footer.php");
+?>
+
