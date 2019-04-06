89388ccfec9c357da5e3eca42064b031d8dad782
Paul Crane <paul@acheronsoft.com>
2019-04-06 00:04:10 -0500

    Add initial header/footer, css and images.

--- pages/footer.php
+++ pages/footer.php
@@ -1,3 +1,3 @@
-<?php
-echo ("</body></html>");
-?>
+</body>
+
+</html>
--- pages/header.php
+++ pages/header.php
@@ -1,4 +1,10 @@
+<html lang="en">
 
-<?php
-echo ("<html><body>");
-?>
+<head>
+	<meta charset="UTF-8">
+	<title>Spring, Summer and no Fall or Winter because they suck. With a little rain mixed in.</title>
+	<link rel="stylesheet" href="css/style.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/howler/2.1.1/howler.js"></script>
+</head>
+
+<body>
